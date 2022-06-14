61859f567239af2e8a4b434aa653745e55ab4fc5
BotSatMagic <104804245+BotSatMagic@users.noreply.github.com>
2022-06-14 10:39:20 -0500

    Update adult-pack2.js

--- plugins/adult-pack2.js
+++ plugins/adult-pack2.js
@@ -1,3 +1,4 @@
+/*
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
@@ -47,3 +48,4 @@ global.packgirl = [
   "https://i.imgur.com/6dXFsBW.jpg",
   "https://i.imgur.com/6CeG9ZX.jpg"
 ]
+*/
